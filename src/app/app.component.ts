c8f72a2fcc05045044fe845956944022595b73de
reemsh2000 <sh951652@gmail.com>
2022-12-20 22:08:43 +0200

    add abbrevations app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'abbrevations';
+  data: any = {};
+  result = '';
+  showResult = false;
+
+  constructor(public dataService: DataService) {}
+  async ngOnInit() {
+    this.data = await this.dataService.getPAbbrevations();
+  }
+
+  search(sentence: any) {
+    this.result=""
+    let shorts = sentence.trim().split(' ');
+    for (let i = 0; i < shorts.length; i++) {
+      let item = shorts[i];
+      this.result += this.data[item] ? this.data[item] + ' ' : shorts[i];
+    }
+    this.showResult = true;
+  }
 }
